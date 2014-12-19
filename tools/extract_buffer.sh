ed1116cd12e07a59e2268afaaf2126d758e5ba39
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2014-12-19 11:30:37 +0100

    modificado extract buffer, pasar fichero desde parametro

--- tools/extract_buffer.sh
+++ tools/extract_buffer.sh
@@ -1,8 +1,41 @@
 #!/bin/sh
 
+filename = "P1.dat"
+
+usage() {
+    echo $0
+    echo "Extract Buffer"
+    echo "  [-f (File, \"$filename\" by default)]"
+    echo "  [-? (help)]"
+}
+
+echo $0: parsing: $@
+
+while getopts "f:?" opt; do
+    case ${opt} in
+	f)
+	    filename="${OPTARG}"
+	    ;;
+	?)
+	    usage
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: -${OPTARG}" >&2
+	    usage
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -${OPTARG} requires an argument." >&2
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+
 #Crear fichero con la mezcla de todos los peers
-echo -n "Enter file name: "
-read filename
+#echo -n "Enter file name: "
+#read filename
 cat $filename | cut -f 2 | grep "note over"
 
 
